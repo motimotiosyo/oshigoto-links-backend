5a085fb6663985b1013305658ddf72463d5630ad
Pe-chan8 <hr.tp.892837@gmail.com>
2025-09-19 07:40:14 +0900

    feat/answers-create

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,10 @@
 class Answer < ApplicationRecord
-  belongs_to :question, counter_cache: true
   belongs_to :user
+  belongs_to :question, counter_cache: true
+  # 質問に対して複数のいいね、ブックマーク、コメント（後で実装）
+  # has_many :question_likes, dependent: :destroy
+  # has_many :question_bookmarks, dependent: :destroy
+  # has_many :question_comments, dependent: :destroy
 
   validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get :ping, to: "pings#show"  # TODO: 開発後に削除
       resources :experience_posts, only: [ :index, :show, :create, :update, :destroy ]
-      resources :questions, only: [ :index, :show, :create, :update, :destroy ]
+      resources :questions, only: [ :index, :show, :create, :update, :destroy ] do
+        resources :answers, only: [ :index, :create, :update, :destroy ]
+    end
 
       # ユーザー登録
       resources :users, only: [ :create ]
